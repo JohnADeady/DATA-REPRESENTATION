bb40236467c30189fcd735b2a8c0f838cb0fab88
John Deady <g00364751@gmit.ie>
2019-10-30 20:21:57 +0000

    Week 5 Correction

--- Week 5/Week05/server/simpleserver.py
+++ Week 5/Week05/server/simpleserver.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!flask/bin/python
 from flask import Flask
 
@@ -11,18 +10,4 @@ def index():
 	return "Hello, World!"
 	
 if __name__ == '__main__':
-=======
-#!flask/bin/python
-from flask import Flask
-
-app = Flask ('__name__',
-			static_url_path='',
-			static_folder='../')
-
-@app.route('/')
-def index():
-	return "Hello, World!"
-	
-if __name__ == '__main__':
->>>>>>> 955aac562b369b9e51df3a0f30a7478d09151948
 	app.run(debug= True)
